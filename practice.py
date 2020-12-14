078befe434b021c14b9d9b358aa588b5da3a22ae
Jongbo <whdqh1550@gmail.com>
2020-12-14 20:23:55 +0900

    함수사용법. return

--- practice.py
+++ practice.py
@@ -5,5 +5,44 @@ from random import *
 # random 모듈의 shuffle과 sample을 활용
 
 
-students = [2,3,4,5]
-print(students)
+def openAccount():
+    print("new account is made.")
+openAccount()
+def deposit(balance,money):
+    print("Deposit completed")
+    print("money on account : {0}".format(balance+money))
+    return balance + money
+def withdraw(balance,money):
+    if money > balance:
+        print("not enough balance")
+        return
+    else : 
+        print("you withdraw {0} from your account ".format(money))
+        print("Left balance {0}".format(balance - money))
+        return balance - money
+
+def withdrawNight(balance , money):
+    commission = 1
+
+    if money > (balance+commission):
+        print("not enough balance")
+        return balance
+    else : 
+        print("you withdraw {0} from your account ".format(money))
+        print("Left balance {0}".format(balance - money-commission))
+        return commission , balance - money - commission
+
+    
+    
+
+
+
+balance = 0
+balance = deposit(balance,200)
+
+balance = deposit(balance,300)
+
+balance = withdraw(balance,300)
+
+commission, balance = withdrawNight(balance,100)# 이렇게 두개의 값을 리턴받고 두개를 한번에 다른 값에 저장시켜줄수가 있음
+
