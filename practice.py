c85ab310c7840c13ad0a6493fd3af98224182dc5
Jongbo <whdqh1550@gmail.com>
2020-12-15 18:10:32 +0900

    정렬하는방법 advnaced,

--- practice.py
+++ practice.py
@@ -1,8 +1,3 @@
-# scores = {"Math":50,"Englis":70,"Code":100}
-
-# for subjects,grade in scores.items():
-  
-#     print(subjects.ljust(8),str(grade).rjust(4),sep=":") # ljust 왼쪽정렬 rjust 오른쪽 정렬 sep = 컴마 로 갈른곳마다 뭐가들어갈까 정렬뒤의 숫자는 총 몇칸중에 좌/우 로 정렬할까.
-
-for i in range(1,21):
-    print("대기번호 : "+ str(i).zfill(3))# zfill 은 zerofill 이다 그옆에 숫자는 몇자리인지 물어보는거다. 숫자가 없는 빈곳에는 zero 로 채워준다. 
+print("{0:0>10}".format(200))
+print("{0:^<+20,}".format(1000000000))#정렬하는방법.3자리마다 콤마 붙이기도 가능함. ^빈자리에 들어가고 < 왼쪽정렬 + 부호 붙여주고 20 자리를 총 사용함.
+print("{0:f}".format(5/3))#소숫점 출력하는방법. 
