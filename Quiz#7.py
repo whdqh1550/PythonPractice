04e112cc704ba81fbc68ab7b3695b55e7a3f64c8
Jongbo <whdqh1550@gmail.com>
2020-12-16 19:14:53 +0900

    파일 자동 생성기.
    파일 자동 생성기.

---
+++ Quiz#7.py
@@ -0,0 +1,6 @@
+
+for i in range(1,51):
+    with open("-{0} 주차 주간 보고-.txt".format(i),"w",encoding="utf8") as report_file:
+        report_file.write("부서: \n")
+        report_file.write("이름: \n")
+        report_file.write("업무 요약: \n")
