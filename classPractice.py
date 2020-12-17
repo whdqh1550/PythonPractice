559ced22e5bdecfb4884c631eee3afc9a2727981
Jongbo <whdqh1550@gmail.com>
2020-12-18 01:33:05 +0900

    스타크래프트 활용 클래스 연습.py

--- classPractice.py
+++ classPractice.py
@@ -1,35 +1,37 @@
 class Unit :
-    def __init__(self, name , hp):
+    def __init__(self,name,hp,speed):
         self.name = name
         self.hp = hp
-        
-        print("{0} has been produced. Hp is {1}".format(self.name,self.hp))
-
-class attackUnit (Unit):
-    def __init__(self,name,hp,damage):
-        Unit.__init__(self,name,hp)# 위로가서 Unit 클래스 init 함수실행후 결과를 여기로 가져와서 다 있다. 
+        self.speed = speed
+        print("{0} 유닛이 생성 되었습니다.".format(self.name))
+    
+    def move(self, location):
+        print("{0} 는 {1}방항으로 움직입니다 [속도{2}] ".format(self.name,location,self.speed))
+    
+class attackUnit(Unit):
+    def __init__(self,name,hp,speed,damage):
+        Unit.__init__(self,name,hp,speed)
         self.damage = damage
-        print("{0} hp : {1}, damage: {2}".format(self.name,self.hp,self.damage))
+        
+    def attack(self,location):
+        print("{0}: {1}방향으로 공격 [공격력: {2}]".format(self.name,location,self.damage))
+class Marine(attackUnit):
+    def __init__(self,name,hp,speed,damage):
+        attackUnit.__init__(self,name,hp,speed,damage)
+        self.stimpack = False
+    def steampack(self):
+        if self.stimpack == False :
+            self.stimpack = True
+            self.damage = self.damage*2
+        else:
+            self.stimpack = False
+            self.damage = self.damage/2
+        print("현재 공격력[{0}]".format(self.damage))
 
-# marine = attackUnit("marine",20,3)   
-# marine2 = attackUnit("marine",20,3)
-# marine2.Steampack = True
-# if marine2.Steampack == True:
-#     print("{0}`s steam pack is on".format(marine2.name))
+m1 = Marine("marine",20,3,4)
+m1.move("1시")
+m1.attack("1시")
+m1.steampack()
+m1.attack("2시")
 
-class flyableUnit :
-    def __init__(self, flyingspeed):
-        self.flyingspeed = flyingspeed
-    def fly(self,location):
-        print("locatoin : {0}, speed {1}".format(location,self.flyingspeed))
 
-# wraith = flyableUnit(20)
-# wraith.fly("1 시")
-
-class flyAttackUnit(attackUnit,flyableUnit) :
-    def __init__(self,name,hp,damage,flyingspeed):
-        attackUnit.__init__(self,name,hp,damage)
-        flyableUnit.__init__(self,flyingspeed)
-
-wraith = flyAttackUnit("wraith",30,8,20)
-wraith.fly("1시")
