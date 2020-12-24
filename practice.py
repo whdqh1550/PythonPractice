014d018302e121fced4f7d141591b8d10f29d13f
whdqh1550 <44498387+whdqh1550@users.noreply.github.com>
2020-12-24 22:53:28 +0900

    pang game and quiz arrangement

--- practice.py
+++
@@ -1,9 +0,0 @@
-# import travel.Thailand
-# sdetail = travel.Thailand.thailand()
-
-# sdetail.detail()
-
-import byme
-
-signiture = byme.Byme()
-signiture.sign()
---
+++ pygame_project/1_frame_background_stage_character.py
@@ -0,0 +1,73 @@
+import pygame
+import os
+
+#################################
+
+pygame.init()# 초기화 반드시 필요함.
+
+
+#화면 크기 설정
+screen_width = 640
+screen_height = 480
+screen = pygame.display.set_mode((screen_width,screen_height))
+
+pygame.display.set_mode((screen_width,screen_height))
+
+#화면 타이틀 설정
+pygame.display.set_caption("pangpng")#게임 이름
+
+#FPS
+clock = pygame.time.Clock()
+#################################
+
+#1. 사용자 게임 초기화(배경 화면, 게임 이미지, 좌표, 폰트,속도, 시간 등)
+current_path = os.path.dirname(__file__)#이렇게 할경우 지금 현제 이 파일이 있는 디렉토리를 저장함.
+image_path = os.path.join(current_path,"image")# 이렇게 current_path 뒤에 image 를 붙임 으로써 current path 안의 이미지 폴더가 image_path 에 저장됨.
+
+background = pygame.image.load(os.path.join(image_path,"background.png"))
+
+stage = pygame.image.load(os.path.join(image_path,"stage.png"))
+stage_size = stage.get_rect().size
+stage_width = stage_size[0]
+stage_height = stage_size[1]
+
+
+
+
+
+
+#이벤트 루프
+running = True #게임이 진행중인가?
+while running:
+    deltaTime = clock.tick(60)#게임 화면의 초당 프레임수를 설정 이걸 선언 하는것만으로도 fps 를 조절함. 
+    # 만약 1 초에 100 을 가야한다고 쳤을때
+    # 10 fps 는 속도 몇으로 가야하는가? 10 이다 한번의 움직임당 10 으로 가야 1초에 100 을 갈수있다
+    # 10fps 는 1초동안 10 번을 동작한다는 것이다.
+    #fps 와 상관 없이 이동 속도를 조절해야한다. 
+
+
+    #2 이벤트 처리 (키보드 , 마우스 등)
+    for event in pygame.event.get(): #이게 사용자의 인풋을 이벤트에 넣어서 작동하게 해준다. pygame.event.get() 은 input 을 받아오는 것이고 event 에 저장해서 게임속 캐릭터에게 전달해준다. 
+        if event.type == pygame.QUIT:# 창이 닫히는 이벤트가 발생 하면 running 을 false 로 만들어 줘서 while loop 을 탈출하고 게임을 종료 하게된다. 
+            running = False
+       
+
+    # 3. 게임캐릭터 위치 정의
+    
+    #4. 충돌 처리
+    
+    
+    #5. 화면에 그리기
+
+    screen.blit(background,(0,0))
+    screen.blit(stage,(0,screen_height-stage_height))
+
+   
+
+    pygame.display.update()# 게임 화면을 계속 다시 그리기. 반드시 들어가야함. 
+
+pygame.time.delay(2000)# 게임끝나고 2초 정도 기달리고 는디ㅏ.
+  
+
+#pygame 종료
+pygame.quit()
